261b72af2b3176c7190c0e849c03660e10df8324
Ilya Vorozhtsov <killmisterlenny@yandex.ru>
2024-03-06 19:51:49 +0300

    chat page layout completed

--- src/pages/chat-page/index.ts
+++ src/pages/chat-page/index.ts
@@ -1,2 +1,113 @@
+import Handlebars from "handlebars";
 import "./chat-page.scss";
 export { default as ChatPage } from "./chat-page.hbs?raw";
+
+Handlebars.registerHelper("left-column-users", () => {
+    return [
+        {
+            name: "Илья", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "2", 
+            image: "", 
+            time: "10:49"
+        },
+        {
+            name: "Петр", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        },
+        {
+            name: "Пользователь", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        },
+        {
+            name: "Пользователь", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        },
+        {
+            name: "Пользователь", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        },
+        {
+            name: "Пользователь", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        },
+        {
+            name: "Пользователь", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        },
+        {
+            name: "Пользователь", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        },
+        {
+            name: "Пользователь", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        },
+        {
+            name: "Пользователь", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        },
+        {
+            name: "Пользователь", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        },
+        {
+            name: "Пользователь", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        },
+        {
+            name: "Пользователь", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        },
+        {
+            name: "Пользователь", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        },
+        {
+            name: "Пользователь", 
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            unread: "", 
+            image: "", 
+            time: "Пн"
+        }
+    ]
+})
