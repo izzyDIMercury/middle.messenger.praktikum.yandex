3c4da13938079790f955ba370474662aa3872680
Ilya Vorozhtsov <killmisterlenny@yandex.ru>
2024-03-09 15:07:07 +0300

    more fixes

--- server.cjs
+++ server.cjs
@@ -4,4 +4,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("./dist"));
 
+app.get("/", (req, res) => {
+    res.status(200);
+    res.send("Random text");
+})
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
