c15b10bbbf189552eb081ab1c675bd0ae25524af
Ilya Vorozhtsov <killmisterlenny@yandex.ru>
2024-08-10 12:59:49 +0300

    fix

--- server.cjs
+++ server.cjs
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("./dist"), { "dotfiles": "allow" });
+app.use(express.static("./dist"), { "redirect": "false" });
+
+// { "dotfiles": "allow" }
 
 app.get("/", (req, res) => {
     res.status(200);
