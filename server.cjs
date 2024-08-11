e2f67ba905a42430ab3a5ed875261961f3106368
Ilya Vorozhtsov <killmisterlenny@yandex.ru>
2024-08-11 21:41:18 +0300

    fix

--- server.cjs
+++ server.cjs
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require("path");
+
 
 app.use(express.static(path.join(__dirname, "dist")));
+
+app.get('*', function(request, response, next) {
+    response.sendfile(path.join(__dirname, "dist") + '/index.html');
+});
 
 app.get("/", (req, res) => {
     res.status(200);
