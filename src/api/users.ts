e8579dfcf1d5a57c4928fdefa45b253089c1688a
Ilya Vorozhtsov <killmisterlenny@yandex.ru>
2024-08-13 08:45:17 +0300

    fix

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,11 +1,12 @@
 import HTTPTransport from "../core/request.ts";
+import type { HTTP, UserData } from "../types.ts";
 
-type RequestData = Record<string, any>
+// type RequestData = Record<string, any>
 
 export default class UsersApi {
 
     private host: string = "https://ya-praktikum.tech/api/v2";
-    private HTTP = new HTTPTransport();
+    private HTTP: HTTP = new HTTPTransport();
 
     public async logout() {
         return this.HTTP.post(`${this.host}/auth/logout`, {
@@ -13,7 +14,7 @@ export default class UsersApi {
         });
     }
 
-    public async changeUserData(userData: RequestData) {
+    public async changeUserData(userData: UserData) {
         return this.HTTP.put(`${this.host}/user/profile`, {
             data: userData,
             headers: {
@@ -22,13 +23,13 @@ export default class UsersApi {
         })
     }
 
-    public async changeUserPassword(userData: RequestData) {
+    public async changeUserPassword(userData: UserData) {
         return this.HTTP.put(`${this.host}/user/password`, {
             data: userData
         })
     }
 
-    public async setUserAvatar(avatar: RequestData) {
+    public async setUserAvatar(avatar: UserData) {
         return this.HTTP.put(`${this.host}/user/profile/avatar`, {
             data: avatar
         });
